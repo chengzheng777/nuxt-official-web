28056cb89c42c5ce7e1d58cf664e916ad3cb491d
chengzheng777 <153415866@qq.com>
2024-01-17 13:44:00 +0800

    add：页头信息

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  // 页头信息
+  app: {
+    head: {
+      charset: 'utf-8', // 快捷方式
+      viewport: 'width=device-width, initial-scale=1', // 快捷方式
+      title: '房讯通',
+      meta: [
+        { name: 'description', content: 'My amazing site.' },
+        { name: 'charset', content: 'utf-8' },
+      ],
+      "link": [],
+      "style": [],
+      "script": []
+    }
+  },
   modules: [
-    '@nuxt/ui',   
+    '@nuxt/ui',
     // 引入 Pinia
     [
       "@pinia/nuxt",
